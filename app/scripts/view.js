a97518d8da9c76565e6da7b97de018d1ba269c7d
Liam Morley <liam@carpeliam.com>
2013-10-13 01:50:54 -0400

    improve mobile UX
    * embiggen text
    * link to directions (should open up Google Maps on phone)
    * optionally hide played pianos

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -1,37 +1,46 @@
-define(['jquery', 'underscore', 'backbone', 'text!templates/list.jst'], function($, _, Backbone, listTmpl) {
+define(['jquery', 'underscore', 'backbone', 'text!templates/list.ejs', 'text!templates/head.html'], function($, _, Backbone, listTmpl, header) {
   'use strict';
   return Backbone.View.extend({
     template: _.template(listTmpl),
     events: {
       'change .piano-cb': 'togglePiano',
-      'click .directions': 'comingSoon'
+      'change #hide': 'showOrHidePlayedPianos'
     },
     initialize: function() {
       this.collection.on('sort', _(this.render).bind(this));
     },
 
     render: function() {
-      this.$el.empty();
       var view = this;
-      this.collection.each(function(piano) {
+      view.$el.html(header);
+      view.collection.each(function(piano) {
         var context = _(piano.toJSON()).extend({id: piano.cid, isPlayed: piano.isPlayed()});
         view.$el.append(view.template(context));
       });
-      return this;
+      return view;
     },
 
     togglePiano: function(e) {
-      var $cb = $(e.target), cid = $cb.data('id'), piano = this.collection.get(cid);
+      var $cb = $(e.target),
+          $row = $cb.closest('.row'),
+          $label = $row.find('label[for=' + $cb.attr('id') + ']'),
+          cid = $cb.data('id'),
+          piano = this.collection.get(cid);
+      $label.toggleClass('text-muted');
       if ($cb.is(':checked')) {
         piano.play();
+        if (this.$('#hide').is(':checked')) {
+          $row.hide();
+        }
       } else {
         piano.unplay();
       }
     },
 
-    comingSoon: function() {
-      alert('Directions coming soon. Maybe.');
-      return false;
+    showOrHidePlayedPianos: function() {
+      var playedPianos = this.collection.select(function(p) { return p.isPlayed(); }),
+          ids = _(playedPianos).map(function(p) { return '#check' + p.cid; });
+      this.$(ids.join()).closest('.row').toggle();
     }
   });
 });
